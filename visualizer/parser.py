d911b6c12ce96edcda0f1d39d978d210a7cdf824
Farzan Dehbashi <farzandb@Farzans-MacBook-Pro.local>
2021-01-28 12:35:44 -0500

    .

--- visualizer/parser.py
+++ visualizer/parser.py
@@ -1,15 +1,19 @@
 import pandas as pd
 import sys
 import os
-
+class tag:
+    def __init__(self, color):
+        self.color = color
 def Read(list_of_gestures):
     data_directory = sys.argv[1]
     with os.scandir("./"+str(data_directory)) as gesture_directories:
         for gesture_directory in gesture_directories:
             if os.path.isdir("./"+str(data_directory)+str(gesture_directory.name)):
-                with os.scandir("./"+str(data_directory)+str(gesture_directory.name)) as gesture_logs:
-                    for gesture_log in gesture_logs:
-                        list_of_gestures.append(gesture_log)
+                with os.scandir("./"+str(data_directory)+str(gesture_directory.name)) as gesture_files:
+                    for gesture_file in gesture_files:
+                        gesture_data = pd.read_csv(str(data_directory)+str(gesture_directory.name)+"/"+str(gesture_file.name))
+                        gesture_data.name = str(gesture_file.name)
+                        list_of_gestures.append(gesture_data)
 
 
 
@@ -19,14 +23,17 @@ def Read(list_of_gestures):
             #            print("scanning file " + str(entry.name))
             #            gesture_data = pd.read_csv(str(data_directory)+str(gesture_directory)+str(entry.name))
             #            return gesture_data
-
-
+def make_tags:
+    instruction_sheet = pd.read_csv("instructions.csv")
 
 def main():
-    gestures = []
-    Read(gestures)
-    for gesture in gestures:
-        print(gesture.to_string())
+    gesture_sheets = []
+    Read(gesture_sheets)
+    make_tags()
+
+
+    for gesture in gesture_sheets:
+        print(gesture.name)
 
 
 
---
+++ visualizer/visualizer.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import sys
+import matplotlib
+
+
+if __name__ == "__main__":
+    
