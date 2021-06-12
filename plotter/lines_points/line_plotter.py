49716bad2bd2b94dc47fe4e70bb44baf5ce051cb
farzandb <f2dehbas@uwaterloo.ca>
2021-06-11 20:15:26 -0400

    flags added

--- plotter/lines_points/line_plotter.py
+++ plotter/lines_points/line_plotter.py
@@ -3,13 +3,9 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv('input.csv')
-
-#df.plot.line(x='Distance from the chip (cm)',y='RSS (dB)', grid=True, title = 'Flat slider (32cm)',linestyle='--', marker='o')
+df.plot.line(x='Distance from the chip (cm)',y='RSS (dB)', grid=True, title = 'Sliding antenna on a full wave-length bended transmission line',linestyle='--', marker='o')
 #'Tag 1 (Half length)','Tag 2 (Half length)'
-#plt.ylim((-70,-35))
-x = np.arange(0,2*np.pi,0.1)   # start,stop,step
-y = np.cos(x)
+plt.ylim((-70,-35))
 
-plt.plot(x,y)
 plt.grid(True)
 plt.show()
